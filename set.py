667b9018d2ec81c8b8cd69f7aa7d03dfcffdf071
Pragati Gupta <praeast94@gmail.com>
2025-11-30 00:37:52 +0530

    practiced set

--- set.py
+++ set.py
@@ -1,2 +1,18 @@
-thisset = {"apple", "banana", "cherry"}
+thisset = {"apple", "banana", "cherry", "banana"}
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+thisset.add("orange")
+
+print(len(thisset))
+print(thisset)
+
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set2.add(6)
+
+set3 = set1.union(set2)
+print(set3)
+fset = frozenset({"apple", "banana", "cherry"})
+print(fset)
+print(type(fset))
 
