cacfbf13fcca525b792eaee2db449f205876d945
Pragati Gupta <praeast94@gmail.com>
2025-11-29 16:48:57 +0530

    Practice programs on basic python

---
+++ hello.py
@@ -0,0 +1,46 @@
+print("hello, world!");
+# import sys
+# print(sys.version)
+if 5 < 2:
+    print("5 is greater than 2");
+    print("5 is greater than 2")
+x = 5
+x = "hello"
+y = "  My Name is Pragati   "
+print(x)
+# print(float(x))
+print(type(x))
+print(y);
+print("Hello",end = " "); print("How are you?"); print("Bye bye!")
+fruits = ["apple", "banana", "cherry"]
+p, q, r = fruits
+print(p)
+print(q)
+import random
+print(random.randrange(1, 1000))
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+print(len(a))
+print("dolor" in a)
+print(a[2:5])
+print(a[2:])
+print(a[:5])
+print(a[-5:-2])
+print(x.upper())
+print(y.strip())
+print(y.replace("a", "L"))
+print(y.split("s"))
+print(x + " " + y)
+age = 35
+print(f"my name is paras and my age is {age}")
+print(10>9)
+print(bool("hello"))
+print(bool(0))
+r = 200
+print(isinstance(r, int))
+print(k := 3)
+fruits = ["apple", "banana", "cherry"]
+print("banana" in fruits) #'in' is an membership operator here
