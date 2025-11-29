a680fedf3ad76f657d8d6f9e534272eedf950078
Pragati Gupta <praeast94@gmail.com>
2025-11-30 00:47:57 +0530

    practice dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,14 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print(thisdict["brand"])
+print(len(thisdict))
+x = thisdict.get("model")
+print(x)
+x = thisdict.keys()
+print(x)
+x = thisdict.values()
+print(x)
