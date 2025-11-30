8f2576db6ae213fab5d3f1b84c16e75962140f0f
Pragati Gupta <praeast94@gmail.com>
2025-11-30 20:09:36 +0530

    while and for loop practiced

---
+++ loop.py
@@ -0,0 +1,56 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+for x in "banana":
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+for x in range(6):
+  print(x)
+print("")
+for x in range(2, 6):
+  print(x)
+print("")
+# Increment the sequence with 3 (default is 1):
+for x in range(2, 30, 3):
+  print(x)
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+# Note: The else block will NOT be executed if the loop is stopped by a break statement.
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+print("")
+# nested loop
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+# for loops cannot be empty, but if you for some reason have a for loop with no content, put in the pass statement to avoid getting an error.
+for x in [0, 1, 2]:
+  pass
