f486153dc1908a9a90c6bb9bf3122a432fd2d914
Pragati Gupta <praeast94@gmail.com>
2025-11-30 18:27:56 +0530

    revise dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -12,3 +12,58 @@ x = thisdict.keys()
 print(x)
 x = thisdict.values()
 print(x)
+
+# Add a new item to the original dictionary, and see that the values list gets updated as well:
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.values()
+print(x) #before the change
+car["color"] = "red"
+print(x) #after the change
+
+# Print all key names in the dictionary, one by one:
+for x in thisdict:
+  print(x)
+
+# Print all values in the dictionary, one by one:
+for x in thisdict:
+  print(thisdict[x])
+
+# You can also use the values() method to return values of a dictionary:
+for x in thisdict.values():
+  print(x)
+
+# Make a copy of a dictionary with the dict() function:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+# Create three dictionaries, then create one dictionary that will contain the other three dictionaries:
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+for x, obj in myfamily.items():
+  print(x)
+  for y in obj:
+    print(y + ':', obj[y])
