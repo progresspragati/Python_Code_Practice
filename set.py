0a8cda2a2a4c190b1fd7fbb73b1b8826132b6fe6
Pragati Gupta <praeast94@gmail.com>
2025-11-30 18:27:23 +0530

    revise set

--- set.py
+++ set.py
@@ -16,3 +16,39 @@ fset = frozenset({"apple", "banana", "cherry"})
 print(fset)
 print(type(fset))
 
+# You cannot access items in a set by referring to an index or a key.
+
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
+
+# You can use the | operator instead of the union() method, and you will get the same result.
+#  The  | operator only allows you to join sets with sets, and not with other data types like you can with the  union() method.
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = set1 | set2
+print(set3)
+print("")
+
+# You can use the - operator instead of the difference() method, and you will get the same result.
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1 - set2
+print(set3)
+# Note: The - operator only allows you to join sets with sets, and not with other data types like you can with the difference() method.
+
+# You can use the ^ operator instead of the symmetric_difference() method, and you will get the same result.
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1 ^ set2
+print(set3)
+# Note: The ^ operator only allows you to join sets with sets, and not with other data types like you can with the symmetric_difference() method.
+
+
+
+
+
+
