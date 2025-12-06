96dcb38a57bf6f6804e85d9acdce5ed9bde36b2c
Pragati Gupta <praeast94@gmail.com>
2025-12-06 13:07:30 +0530

    I have completed all three phases of guessing game.

--- myFirstGame.py
+++ myFirstGame.py
@@ -1,22 +1,32 @@
 import random
-random_num = random.randrange(1,10)
-print("**************Welcome!**************\nGuess The Number Between 1 to 10.")
-print("************************************")
-i = 0
-while i < 5:
-    try:
-        guess_num = int(input("Enter Your Guess Number:"))
-    except:
-        print("Given input is not a number!")
-    if guess_num == random_num:
-        print("Congratulations! You won the Game!")
-        exit()
-    else:
-        if guess_num > random_num:
-            print("Guessed number is too high")
+def guessingNumberGame():
+    random_num = random.randrange(1,10)
+    print("**************Welcome!**************\nGuess The Number Between 1 to 10.")
+    print("************************************")
+    i = 1
+    j = 0
+    while i <= 5 and j == 0:
+        try:
+            guess_num = int(input("Enter Your Guess Number:"))
+        except:
+            print("Given input is not a number!")
+        if guess_num == random_num:
+            print(f"You guessed right number in {i} attempts.")
+            print("Congratulations! You won the Game!")
+            j = 1
         else:
-            print("Guessed number is too low")
-        print("Please Try Again!")
-    i += 1
-print("Your have tried Maximum Attempts, You have lost the Game!")
-print(f"The correct number is {random_num}")
+            if guess_num > random_num:
+                print("Guessed number is too high")
+            else:
+                print("Guessed number is too low")
+            print("Please Try Again!")
+        i += 1
+    if j == 0:
+        print("Your have tried Maximum Attempts, You have lost the Game!")
+        print(f"The correct number is {random_num}")
+    is_continue_game = input("Do you want to play again?(y/n):")
+    if is_continue_game == "y":
+        print("")
+        guessingNumberGame()
+    return
+guessingNumberGame()
