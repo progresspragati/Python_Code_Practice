4ff27910a9b38420a9e87e724ba554b7e36a6dd3
Pragati Gupta <praeast94@gmail.com>
2025-12-07 15:17:24 +0530

    Optimise code, remove recursion, and used a game loop, optimise ui/ux.

--- myFirstGame.py
+++ myFirstGame.py
@@ -1,37 +1,40 @@
-MAX_ATTEMPTS = 3
+import random
+
+MAX_ATTEMPTS = 10
 LOW, HIGH = 1, 100
 
-import random
 def guessingNumberGame():
-    random_num = random.randrange(LOW, HIGH)
-    print(f"**************Welcome!**************\nGuess The Number Between {LOW} to {HIGH}.")
-    print("************************************")
-    i = 1
-    j = 0
-    while i <= MAX_ATTEMPTS and j == 0:
-        try:
-            guess_num = int(input("Enter Your Guess Number:"))
-        except:
-            print("Given input is not a number!Please, enter a number!")
-            continue
-        if guess_num == random_num:
-            print(f"You guessed right number in {i} attempts.")
-            print("Congratulations! You won the Game!")
-            j = 1
-        else:
-            if guess_num > random_num:
-                print("Guessed number is too high")
+    is_continue_game = "y"
+    while is_continue_game == "y":
+        random_num = random.randint(LOW, HIGH)
+        print(f"\n**************Welcome!**************\nGuess The Number Between {LOW} to {HIGH}.")
+        print("************************************\n")
+        attempts = 1
+        won = False
+        while attempts <= MAX_ATTEMPTS and not won:
+            try:
+                guess_num = int(input("Enter Your Guess Number:\n-> "))
+            except ValueError:
+                print("Given input is not a number!Please, enter a number!")
+                continue
+            if guess_num < LOW or guess_num > HIGH:
+                print(f"Given input is out of range.Please, Enter the number between {LOW} and {HIGH}.")
+                continue
+            if guess_num == random_num:
+                print(f"You guessed right number in {attempts} attempts.")
+                print("\nCongratulations! You won the Game!")
+                won = True
             else:
-                print("Guessed number is too low")
-            print("Please Try Again!")
-        i += 1
-    if j == 0:
-        print("Your have tried Maximum Attempts, You have lost the Game!")
-        print(f"The correct number is {random_num}")
-    is_continue_game = input("Do you want to play again?(y/n):")
-    if is_continue_game == "y":
-        print("")
-        guessingNumberGame()
+                if guess_num > random_num:
+                    print("Guessed number is too high")
+                else:
+                    print("Guessed number is too low")
+                print(f"Please Try Again! You have left {MAX_ATTEMPTS - attempts} attempts.")
+            attempts += 1
+        if not won:
+            print("You have tried maximum attempts, You have lost the Game!")
+            print(f"The correct number is {random_num}")
+        is_continue_game = input("\nDo you want to play again?(y/n):").strip().lower()
     return
 
 
